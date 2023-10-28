063deb76394a3f01459a3ac5023fee948e9f258e
maymikb <123998301+maymikb@users.noreply.github.com>
2023-10-27 19:58:18 -0700

    added questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,47 @@
+var inquirer= require ("inquirer");
+inquirer
+.prompt([
+    {
+    type: "input",
+    name: "title",
+    message:"What is the title?"
+},
 
-//title of the README
-//description
-//installation instructions
-//usage information
-//contribution guidelines
-//test instructions
+{
+    type: "input",
+    name: "description",
+    message:"What is the description?"
+},
+
+{
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions?",
+},
+
+{
+    type: "input",
+    name: "usage info ",
+    message: "What is the usage information?"
+},
+
+{
+    type: "input",
+    name: "contribution",
+    message: "What are the contribution guidelines?",
+},
+
+{
+    type: "input",
+    name: "testing",
+    message: "What are the test instructions?",
+},
+
+ 
+])
+.then((answers)=>{
+//ANSWERS(FEEDBACK)
+})
 
 
 
